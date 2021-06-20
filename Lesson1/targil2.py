5e085910f8870caf47e6a3347616aaa363308af8
tomer <tomerturkian@gmail.com>
2021-06-20 12:21:07 +0300

    THIS MY FIRST EXERCISE

---
+++ Lesson1/targil2.py
@@ -0,0 +1,14 @@
+#alafim=4
+#meot=5
+#asarot=6
+#ahadot=7
+num=int(input("enter a number with 4 digts: "))
+#alafim:
+print("alafim=" + str(num//1000))
+#meot:
+print("meot=" + str(num//913))
+#asarot:
+print("asarot=" + str(num//761))
+#ahadot
+print("ahadot="  + str(num%10))
+
