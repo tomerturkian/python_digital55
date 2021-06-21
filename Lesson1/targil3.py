f698e37d9fb3428aac4447436ba1f7618e1bb0b7
tomer <tomerturkian@gmail.com>
2021-06-21 16:12:27 +0300

    LAB3-TARGIL

---
+++ Lesson1/targil3.py
@@ -0,0 +1,10 @@
+#LAB3-TARGIL
+
+name="Tomer Turkian"
+mail="tomerturkian@gmail.com"
+age=37
+print("full name: " + name + "\nmail: " + mail + "\nAge: " + str(age))
+
+print("full name: " + (name [::-1]) + "\nAge: " + str(age*3))
+
+print("tomer" in  "idan ben dudu yuval shimon gal adam shahar yana")
