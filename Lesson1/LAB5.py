5d17ecbbacdce33f4b96fbdbad4a677fbe163658
tomer <tomerturkian@gmail.com>
2021-06-24 23:05:36 +0300

    LAB4

---
+++ Lesson1/LAB5.py
@@ -0,0 +1,10 @@
+my_dict={"tomer":100000,"idan":75000,"ben":50000,"dana":25000,"dudu":5000}
+print(my_dict)
+print("The summary is: " + str(my_dict["tomer"]+my_dict["dudu"]))
+
+my_dict["gal"]=400000
+print(my_dict)
+
+print("number of enteris: " +str(len(my_dict)))
+print("idan" in my_dict)
+
